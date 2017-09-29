8090a0eefe5f037c8170366a1ea6bfe053d48d32
aatchison <arron.atchison@gmail.com>
2017-09-29 07:51:27 +0000

    added required patch level of -1 to debian package name

--- package_deb.sh
+++ package_deb.sh
@@ -9,7 +9,7 @@ install_dir="usr/local/bin"
 init_script_location="etc/init.d"
 
 arch="$(dpkg --print-architecture)"
-pkg_name="${pkg_title}_${arch}_${version}"
+pkg_name="${pkg_title}_${arch}_${version}-1"
 root="build/$pkg_name"
 control_file="$root/DEBIAN/control"
 

b90499c17053e2c222daa421a774ad649bcdeef7
aatchison <arron.atchison@gmail.com>
2017-09-29 07:22:59 +0000

    make sure arch is set; right now hardcoded for arm....

--- publish.sh
+++ publish.sh
@@ -19,10 +19,11 @@ function _run() {
 ./package_deb.sh
 
 # this version number is not based on any reality, needs to be changed
+ARCH=armhf
 VERSION=0.1.0
 # upload to s3
-
+cd ./dist
 _run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public . s3://bootstrap.mycroft.ai/artifacts/apt/daily/${ARCH}/wifi-setup-client/${VERSION}/
 echo ${VERSION} > latest
-_run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public ${MYCROFT_CORE_SRC}/dist/latest s3://bootstrap.mycroft.ai/artifacts/apt/daily/${ARCH}/wifi-setup-client/latest
+_run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public ./latest s3://bootstrap.mycroft.ai/artifacts/apt/daily/${ARCH}/wifi-setup-client/latest
 

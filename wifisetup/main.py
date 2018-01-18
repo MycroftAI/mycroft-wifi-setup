1b996d1f7013a86769b40c6cbc1244fa3b889b75
penrods <github@gpll.org>
2018-01-17 20:48:35 -0600

    Extend admin service to support NTP and reboots
    * Rename admin "mycroft." messagebus messages to "system." prefixes.  Now they
    are:
        system.wifi.setup
        system.wifi.reset
        system.ssh.enable
        system.ssh.disable
      NOTE: Old messages still handled for backwards compat
    * Add the following messages to the mycroft-admin service
        system.ntp.sync - forces an NTP clock synchronization
        system.reboot - triggers a system reboot
        system.shutdown - triggers a system shutdown
        system.update - triggers a mycroft-core/platform code upgrade
    * Fix some PEP8 errors.

--- wifisetup/main.py
+++ wifisetup/main.py
@@ -42,7 +42,8 @@ root.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler(sys.stderr)
 ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 root.addHandler(ch)
 
